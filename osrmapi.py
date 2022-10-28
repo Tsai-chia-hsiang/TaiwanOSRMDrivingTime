d50ac74ef6de7bf1ff62f78811a269d9b9264a32
Tsai-chia-hsiang <u08410098@ccu.edu.tw>
2022-10-28 15:44:59 +0800

    adding weakness

--- osrmapi.py
+++ osrmapi.py
@@ -5,10 +5,16 @@ from tqdm import tqdm
 
 class Router():
     
-    def __init__(self):
-        self.client = OSRM(
-            base_url="https://routing.openstreetmap.de/routed-car"
-        )
+    def __init__(self, base_url="https://routing.openstreetmap.de/routed-car"):
+        
+        """
+        Please go to check the document of routingpy 
+        https://routingpy.readthedocs.io/en/latest/#osrm
+        OSRM part 
+        and then setting the ```base_url```
+        """
+        
+        self.client = OSRM(base_url=base_url)
         self.__pbar = None
 
     def _gen_batch(self, nodedict:dict, batchsize)->tuple:
@@ -79,6 +85,17 @@ class Router():
         }
         ```
         
+
+        ### Note:
+        I am using batch to get single source all pair distance, but it
+        is not the fastest method.
+
+        Each query of ```OSRM().matrix``` will return the all
+        pair of querying list, but it has a max length limit.
+
+        For the purpose of easily managing, I choose 
+        single source all pair method
+        although it will be slower.
         """
         idslices , cooslices= self._gen_batch(nodedict, batchsize)
         self.__pbar = tqdm(list(nodedict.keys()))
