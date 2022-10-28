b4453747fdb68b9e7e7daa3d716be31c2aa87d05
Tsai-chia-hsiang <u08410098@ccu.edu.tw>
2022-10-28 15:46:07 +0800

    adding TW main island pair

--- calculate_dist.py
+++ calculate_dist.py
@@ -3,6 +3,7 @@ from dicttool import *
 from attractiondata import AttractionDataset as attrd
 from osrmapi import Router
 
+
 def each_region():
 
     attractions = attrd(
@@ -30,9 +31,16 @@ def taiwan_main_island():
         csvfilepath=os.path.join(".", "cluster_with_coo.csv"),
         region=loadjson(jsonfilepath=os.path.join(".","tw_region.json"))
     )
+
+
     outlying = ["Penghu", "Kinmen","Lienchiang","Greenisland", "Lanyu"]
     main_island = list(t for t in attractions.twregion.keys() if t not in outlying )
     pid_coo = attractions.extract_places_coordinate(target_region=main_island)
+    writejson(pid_coo, os.path.join(".", "mainisland.json"))
+    c=input("calculate all pair ? Total may need 19 hours for all pair.\nType \"R\" to discard.")
+    if c == "R":
+        return 
+    
     savedir = os.path.join("osmdist", "Allpair")
     if not os.path.exists(savedir):
         os.mkdir(savedir)
@@ -41,4 +49,5 @@ def taiwan_main_island():
     router.routing(pid_coo, saveingdir=savedir,batchsize=250)
 
 if __name__ == "__main__":
-    taiwan_main_island()
+    #taiwan_main_island()
+    each_region()
